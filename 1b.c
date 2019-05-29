ed039919f532b6ab6681201362182339e8af8e12
fatalcoder524 <surajpai524@gmail.com>
2019-05-30 02:53:36 +0530

    Update 1b.c

--- 1b.c
+++ 1b.c
@@ -23,9 +23,7 @@ input:exp {printf("%d\n",$$);exit(0);}
 exp:exp'+'exp {$$=$1+$3;}
 |exp'-'exp{$$=$1-$3;}
 |exp'*'exp{$$=$1*$3;}
-|exp'/'exp { if($3==0){printf("Divide by Zero\n");exit(0);}
- else
- $$=$1/$3;}
+|exp'/'exp { if($3==0){printf("Divide by Zero\n");exit(0);}else $$=$1/$3;}
 |'('exp')'{$$=$2;}
 |num{$$=$1;};
 %%
